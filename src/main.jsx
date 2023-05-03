f49172a161f5d63ff7d36d7f6557e0e929ee9c72
yudhaapermana <pyudha0902@gmail.com>
2023-05-03 17:55:41 +0700

    add assets, components & Home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Layout from './templates/Layout'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.json'
+import 'bootstrap/dist/js/bootstrap'
+import './assets/css/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <BrowserRouter>    
+        <Layout />
+      </BrowserRouter>
+    </React.StrictMode>,
 )
