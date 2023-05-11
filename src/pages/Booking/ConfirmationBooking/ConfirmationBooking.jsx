0f5b98de51861c503a0f03fd6b84fe38f6098105
yudhaapermana <pyudha0902@gmail.com>
2023-05-11 23:55:14 +0700

    add mutation delete booking

--- src/pages/Booking/ConfirmationBooking/ConfirmationBooking.jsx
+++ src/pages/Booking/ConfirmationBooking/ConfirmationBooking.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import { gql, useMutation } from '@apollo/client';
 import { useNavigate, useParams } from 'react-router-dom'
-import { HiOutlineArrowNarrowLeft } from 'react-icons/hi'
+import { HiOutlineX } from "react-icons/hi";
 
 import Button from '../../../elements/Button/Button'
 import ListDetailBooking from '../../../components/List/ListDetailBooking'
 import './ConfirmationBooking.css'
 
+const DELETE_BOOKING = gql`
+     mutation MyMutation($id: uuid!) {
+          delete_booking_by_pk(id: $id) {
+               id
+          }
+     }
+`
+
 const ConfirmationBooking = () => {
+     const [deleteBooking] = useMutation(DELETE_BOOKING)
+
      const navigate = useNavigate()     
-     const { id_booking } = useParams()                     
+     const { id_booking } = useParams()
+     
+     const deleteItem = (idx) => {
+          deleteBooking({variables: {
+               id: idx
+          }})
+          location.replace('/create-booking')
+     }
 
      return (
           <section className='confirmation-booking'>
@@ -16,12 +34,17 @@ const ConfirmationBooking = () => {
                     <div className="row justify-content-center">
                          <div className="col-lg-8 col-12">
                               <div className="bg-white text-black mx-auto px-md-5 px-4 pt-4 pt-md-4 pb-5">
-                                   <div className="col-md-11 my-4 my-md-5 mx-auto">
+                                   <div className="col-md-11 my-4 my-md-5 mx-auto ">
+                                        <div className="text-end">
                                         <Button
-                                             classButton={'btn p-0 mb-3 text-start'}
-                                             label={<HiOutlineArrowNarrowLeft className='icon-back text-dark'/>}
-                                             onClick={() => navigate('/create-booking')}
+                                             classButton={'btn p-0 mb-3'}
+                                             label={<HiOutlineX className='icon-back text-dark'/>}
+                                             onClick={() => {
+                                                  window.confirm("yakin ingin membatalkan proses boking")?
+                                                  deleteItem(id_booking): ""
+                                             }}
                                         />
+                                        </div>
                                         <div className="row justify-content-center">
                                              <ListDetailBooking id={id_booking}/>
                                         </div>                                        
